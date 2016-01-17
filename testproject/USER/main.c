fb94a4c6701a5717e6ad90bc4fb6044179862d50
alexwang <188101696@qq.com>
2016-01-18 00:55:02 +0800

    [alex][20160118]add TIM3PWM

--- testproject/USER/main.c
+++ testproject/USER/main.c
@@ -2,6 +2,9 @@
 #include "stm32f10x.h"
  int main(void)
  {	
-
+	 	delay_init();	    	 //延时函数初始化	  
+	  NVIC_Configuration(); 	 //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	  TIM3_PWM_Init(256,0);
+	  TIM_SetCompare2(TIM3,100);	
  }
 
