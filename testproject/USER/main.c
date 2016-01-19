cec2dac499c798e0db61abdc2b9309e98fcc05f8
Alex1 Wang(王寧斌_華碩蘇州) <Alex1_Wang@asuscn.corpnet.asus>
2016-01-19 21:59:55 +0800

    [alex][20160119]add Crazepony.pdf

--- testproject/USER/main.c
+++ testproject/USER/main.c
@@ -4,11 +4,19 @@
 #include "MPU6050.h"
  int main(void)
  {	
+	  int mpu6050flag;
 	 	delay_init();	    	 //延时函数初始化	  
 	  NVIC_Configuration(); 	 //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
 	  TIM3_PWM_Init(256,0);
 	  TIM_SetCompare2(TIM3,100);	
 	  TIM4_PWM_Init(256,0);
 	  TIM_SetCompare2(TIM4,100);
+	  MPU6050_Initialize();
+	  mpu6050flag=MPU6050_TestConnection();
+	  if(!mpu6050flag){
+			return -1;
+		}
+	  
+	 
  }
 
