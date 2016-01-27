5c98a10d82ef75f9d23cbde3d21835fa64888cd5
Alex1 Wang(王寧斌_華碩蘇州) <Alex1_Wang@asuscn.corpnet.asus>
2016-01-27 22:11:33 +0800

    [alex][20160127]add hardware file

--- testproject/USER/main.c
+++ testproject/USER/main.c
@@ -2,21 +2,27 @@
 #include "stm32f10x.h"
 #include "timer.h"
 #include "MPU6050.h"
+#include "usart.h"
  int main(void)
  {	
 	  int mpu6050flag;
+	  u16 times=0;
 	 	delay_init();	    	 //延时函数初始化	  
 	  NVIC_Configuration(); 	 //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	 
 	  TIM3_PWM_Init(256,0);
 	  TIM_SetCompare2(TIM3,100);	
 	  TIM4_PWM_Init(256,0);
 	  TIM_SetCompare2(TIM4,100);
-	  MPU6050_Initialize();
-	  mpu6050flag=MPU6050_TestConnection();
-	  if(!mpu6050flag){
-			return -1;
+	  //init uart+++
+	  uart_init(9600);	 //串口初始化为9600
+	 printf("tianya\n");
+	  while(1){
+			times++;
+			if(times%200==0)printf("hello world\n");
+			delay_ms(10);
+			MPU6050_Initialize();
 		}
-	  
 	 
  }
 
