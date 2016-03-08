d3500bc93d0ef2b3c602276687dfa5ff1e9581e7
Wang Alex <Wang Alex>
2016-03-08 09:24:41 +0800

    [alex][20160308][EE]fix BT

--- testproject/BSP/Led/led.c
+++ testproject/BSP/Led/led.c
@@ -19,10 +19,10 @@ void LED_Init(void)
  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //推挽输出
  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 //IO口速度为50MHz
  GPIO_Init(GPIOA, &GPIO_InitStructure);					 //根据设定参数初始化GPIOA.11 GPIO.12
- GPIO_SetBits(GPIOA,GPIO_Pin_11 | GPIO_Pin_12);						 //GPIOA.11 GPIO.12 输出高
+ GPIO_SetBits(GPIOA,GPIO_Pin_8 | GPIO_Pin_11);						 //GPIOA.11 GPIO.12 输出高
 
- GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6;	    		 //LED3-->PB.5 LED3-->PB.6 端口配置, 推挽输出
+ GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;	    		 //LED3-->PB.5 LED3-->PB.6 端口配置, 推挽输出
  GPIO_Init(GPIOB, &GPIO_InitStructure);	  				 //推挽输出 ，IO口速度为50MHz
- GPIO_SetBits(GPIOB,GPIO_Pin_5 | GPIO_Pin_6); 						 //PB.5 PB.6 输出高 
+ GPIO_SetBits(GPIOB,GPIO_Pin_6 | GPIO_Pin_7); 						 //PB.5 PB.6 输出高 
 }
  
---
+++ testproject/BSP/h/led.h
@@ -0,0 +1,13 @@
+#ifndef __LED_H
+#define __LED_H	 
+#include "sys.h"
+
+#define LED1 PAout(8)// PA8
+#define LED2 PAout(11)// PA11
+#define LED3 PBout(5)// PB5
+#define LED4 PBout(6)// PB6
+
+void LED_Init(void);//≥ı ºªØ
+
+		 				    
+#endif
